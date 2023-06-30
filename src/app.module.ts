f5abe095ef8289f55305635b636956a588267b6a
Romulo Rocha <romulo.rocha@airnz.co.nz>
2023-06-30 22:34:48 +1200

    Creating CRUDs for Users, Products and Posts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProductsModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
