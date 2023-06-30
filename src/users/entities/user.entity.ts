1151af2e30c6faa0ebd0036795c2ca4416d7b0cf
Romulo Rocha <romulo.rocha@airnz.co.nz>
2023-06-30 23:35:57 +1200

    Simple login method draft

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,7 @@
-export class User {}
+export class User {
+    userId:  number;
+
+    username: string;
+
+    password: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  
+  private readonly users: User[] = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: 'guess',
+    },
+  ];
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -14,6 +29,10 @@ export class UsersService {
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
+  }
+
+  findByUsername(username: string) {
+    return this.users.find(user => user.username === username);
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
