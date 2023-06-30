991c4c0f20f78d1aeefea62f46cacb31a758083b
Romulo Rocha <romulo.rocha@airnz.co.nz>
2023-07-01 00:40:00 +1200

    Applying authorization too all resources unless is loging

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,22 +7,21 @@ import {
     HttpCode,
     HttpStatus,
     Post,
-    Request,
-    UseGuards
+    Request
   } from '@nestjs/common';
-  import { AuthGuard } from './auth.guard';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(@Body() signInDto: Record<string, any>) { // TODO - Change to Object User and add validation
     return this.authService.signIn(signInDto.username, signInDto.password);
   }
 
-  @UseGuards(AuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,17 +4,24 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { jwtConstants } from './constants';
 import { JwtModule } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [UsersModule,
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '10m' },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, 
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   exports: [AuthService]
 })
 export class AuthModule {}
