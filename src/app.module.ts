5617dc541b7c9278c9a03efa8da06568609bb7ed
Romulo Rocha <romulo.rocha@airnz.co.nz>
2023-06-30 22:48:46 +1200

    Adding Auth resource empty

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { PostsModule } from './posts/posts.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, PostsModule],
+  imports: [UsersModule, ProductsModule, PostsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
