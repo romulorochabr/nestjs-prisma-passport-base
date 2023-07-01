78af2740c2be913b82fd2f9e0ab802b930bd8b10
Romulo Rocha <romulo.rocha@airnz.co.nz>
2023-07-02 01:18:55 +1200

    Enabling versioning

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('v1');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+    
   const config = new DocumentBuilder()
     .addBearerAuth()
     .setTitle('Para Todos')
